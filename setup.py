0a3c36d3fe2d3012add55142cf4366d41230ef23
Laurent Gautier <lgautier@gmail.com>
2019-06-04 20:07:59 -0400

    Add setup script to allow build/install as a Python package. (#6)

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tablecloth',
+      version='0.0.1',
+      description='SQL queries and tables',
+      author='Laurent Gautier, Jesse Johnson',
+      # author_email='',
+      url='https://github.com/lgautier/tablecloth',
+      packages=['tablecloth'],
+     )
