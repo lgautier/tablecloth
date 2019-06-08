28d93558d038f8c4e67b3c3aaaa28ed29e386e2a
Laurent Gautier <lgautier@gmail.com>
2019-06-08 08:21:43 -0400

    Fix units and trivial implementation issues.
    This is temporarily introducing an inelegant design where the name
    of a query template must be specified twice. This is an unfortunate
    byproduct of the way I manage to gradually introduce the proposed API
    and design changes. This is expected to be resolved over the next few
    PRs.

--- tablecloth/test_queryspace.py
+++ tablecloth/test_queryspace.py
@@ -16,9 +16,11 @@ def test_homogenize():
 def test_compile_single_node():
     space = QuerySpace()
 
-    space['my_query'] = QueryTemplate("""
-    SELECT *
-    FROM {{my_table}}""")
+    space['my_query'] = QueryTemplate(
+        'my_query',
+        """
+        SELECT *
+        FROM {{my_table}}""")
 
     query = space.compile(
         'my_query', {'my_table': 'source_table'})
@@ -32,10 +34,12 @@ FROM source_table
 def test_name_not_found():
     space = QuerySpace()
 
-    space['my_query'] = '''
-SELECT *
-FROM {{my_table}}
-'''
+    space['my_query'] = QueryTemplate(
+        'my_query',
+        """
+        SELECT *
+        FROM {{my_table}}
+        """)
 
     with pytest.raises(NameNotFoundError):
         assert space.compile('my_query', {})
@@ -43,9 +47,9 @@ FROM {{my_table}}
 
 def test_multiple_nodes():
     space = QuerySpace()
-    space['query2'] = QueryTemplate('SELECT * FROM {{query1}}')
-    space['query1'] = QueryTemplate('SELECT * FROM {{my_table}}')
-    space['query3'] = QueryTemplate('SELECT * FROM {{query2}}')
+    space['query2'] = QueryTemplate('query2', 'SELECT * FROM {{query1}}')
+    space['query1'] = QueryTemplate('query1', 'SELECT * FROM {{my_table}}')
+    space['query3'] = QueryTemplate('query3', 'SELECT * FROM {{query2}}')
 
     query = space.compile(
         'query3', {'my_table': 'source_table'})
@@ -65,8 +69,8 @@ SELECT * FROM source_table''')
 
 def test_template_parameters():
     space = QuerySpace()
-    space['query1'] = QueryTemplate('SELECT {v1} FROM {{my_table}}')
-    space['query2'] = QueryTemplate('SELECT {v2} FROM {{query1}}')
+    space['query1'] = QueryTemplate('query1', 'SELECT {v1} FROM {{my_table}}')
+    space['query2'] = QueryTemplate('query2', 'SELECT {v2} FROM {{query1}}')
 
     query = space.compile(
         'query2', {'my_table': 'source_table'},
@@ -80,10 +84,10 @@ SELECT variable_2 FROM query1''')
 
 def test_reuse_previously_seen_nodes():
     space = QuerySpace()
-    space['query2'] = QueryTemplate('SELECT * FROM {{query1}}')
-    space['query1'] = QueryTemplate('SELECT * FROM {{my_table}}')
-    space['query3'] = QueryTemplate('SELECT * FROM {{query1}}')
-    space['query4'] = QueryTemplate('SELECT * FROM {{query2}} JOIN {{query3}}')
+    space['query2'] = QueryTemplate('query2', 'SELECT * FROM {{query1}}')
+    space['query1'] = QueryTemplate('query1', 'SELECT * FROM {{my_table}}')
+    space['query3'] = QueryTemplate('query3', 'SELECT * FROM {{query1}}')
+    space['query4'] = QueryTemplate('query4', 'SELECT * FROM {{query2}} JOIN {{query3}}')
 
     query = space.compile(
         'query4', {'my_table': 'source_table'})
@@ -98,8 +102,8 @@ SELECT * FROM query2 JOIN query3''')
 
 def test_detect_space_cycles():
     space = QuerySpace()
-    space['query1'] = QueryTemplate('SELECT * FROM {{query2}}')
-    space['query2'] = QueryTemplate('SELECT * FROM {{query3}}')
+    space['query1'] = QueryTemplate('query1', 'SELECT * FROM {{query2}}')
+    space['query2'] = QueryTemplate('query2', 'SELECT * FROM {{query3}}')
 
     with pytest.raises(CyclicDependencyError):
-        space['query3'] = QueryTemplate('SELECT * FROM {{query1}}')
+        space['query3'] = QueryTemplate('query3', 'SELECT * FROM {{query1}}')
